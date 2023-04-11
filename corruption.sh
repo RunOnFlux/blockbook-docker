383689b36a86cdff67d20770fe6a3e056aea12ee
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-04-11 22:45:38 +0200

    fix file name

--- corruption.sh
+++ corruption.sh
@@ -30,7 +30,7 @@ if [[ -f /root/blockbook.log ]]; then
   echo -e "----------------------------------------------------------------[END]"
 fi
 
-if [[ -f /root/$CONFIG_DIR/backend/$COIN.log ]]; then
+if [[ -f /root/$CONFIG_DIR/backend/debug.log ]]; then
   echo -e "------------------------------------------ [$(date '+%Y-%m-%d %H:%M:%S')][START]"
   echo -e "| Checking backend logs...."
   corruption=$(grep -ao "Corrupted block database detected" /root/$CONFIG_DIR/backend/$COIN.log)
